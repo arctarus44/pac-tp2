53e7d0938462ce43e1eb9c061c75e5a392cfc915
arctarus44 <arthur.dewarumez@gmail.com>
2015-03-04 13:58:11 +0100

    Sauvegarde du travail.

--- padding-attack/padding-attack.py
+++ padding-attack/padding-attack.py
@@ -1,2 +1,32 @@
+import sys
+import os
+import os.path
+
 sys.path.append(os.path.join(os.getcwd(), '..',))
 import client as clt
+
+seed = "8bd4ae7cc3"
+name = "dewarumez"
+base_url = "http://pac.bouillaguet.info/TP2"
+padding = "/padding-attack"
+challenge = padding + "/challenge/" + name + "/" + seed
+name = "dewarumez"
+
+ch_cipher_text = "ciphertext"
+ch_iv = "IV"
+
+def print_serverError_exit(err):
+	print("err [{0}] : {1} : ".format(err.code, err.msg))
+
+if __name__ == "__main__":
+
+	srv = clt.Server(base_url)
+
+	# Récupération du challenge
+	try:
+		challenge = srv.query(challenge)
+	except clt.ServerError as err:
+		print_serverError_exit(err)
+
+	cipher_text = challenge[ch_cipher_text]
+	init_vector = challenge[ch_iv]
